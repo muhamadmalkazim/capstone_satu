d4158f3ec6e952f9a7e37363c32ce0e6f77bfb57
muhamadmalkazim <129309129+muhamadmalkazim@users.noreply.github.com>
2023-05-03 18:59:12 +0700

    Update tes-capstone.py

--- tes-capstone.py
+++ tes-capstone.py
@@ -1,59 +1,60 @@
 import string
 
-
-# MENU SHOW MAHASISWA TAMBAHIN KEMBALI KE MENU UTAMA
-# MENU 1 DAN 2 HILANG KETIKA KEMBALI KE MENU UTAMA
-
 mahasiswa = [
-    ['Muhamad','Teknik Informatika','B0121455','Ramdani', 3.95],
-    ['Ali','Sistem Informasi','B0121667','Rozi', 3.77],
-    ['Dodi','Psikologi','B0121898','Farhan', 3.88]
+    ['Muhamad','Teknik Informatika','20121455','Ramdani', 3.95],
+    ['Ali','Sistem Informasi','20121667','Rozi', 3.77],
+    ['Dodi','Psikologi','20121898','Farhan', 3.88]
 ]
 
+
 def showMahasiswa():
-    print('''
-    1. Tampilkan Semua Data
-    2. Tampilkan Data Melali NIM
-    ''')
     while True:
-        try:
-            menu_pilih_show = int(input('Pilih Menu: '))
-            break
-        except ValueError:
-            print('Input harus berupa angka')
-    if menu_pilih_show == 1:
-        print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format('NIM', 'NAMA', 'JURUSAN', 'NAMA WALI', 'GPA'))
-        for i in range(len(mahasiswa)):
-            print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format(mahasiswa[i][2], mahasiswa[i][0], mahasiswa[i][1], mahasiswa[i][3], mahasiswa[i][4]))
-    elif menu_pilih_show == 2:
-        nim = input('Masukan NIM: ')
-        for i in range(len(mahasiswa)):
-            if nim == mahasiswa[i][2]:
-                print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format('NIM', 'NAMA', 'JURUSAN', 'NAMA WALI', 'GPA'))
+        print(f'''
+1. Show All Data
+2. Show data By NIM
+3. Kembali ke Menu Utama''')
+        menu_pilih_show = input('Pilih Menu (1/2/3): ')
+
+        if menu_pilih_show == '1':
+            print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format('NIM', 'NAMA', 'JURUSAN', 'NAMA WALI', 'GPA'))
+            for i in range(len(mahasiswa)):
                 print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format(mahasiswa[i][2], mahasiswa[i][0], mahasiswa[i][1], mahasiswa[i][3], mahasiswa[i][4]))
+        elif menu_pilih_show == '2':
+            nim = input('Masukan NIM: ')
+            for i in range(len(mahasiswa)):
+                if nim == mahasiswa[i][2]:
+                    print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format('NIM', 'NAMA', 'JURUSAN', 'NAMA WALI', 'GPA'))
+                    print('{:<10} | {:<30} | {:<30} | {:<30} | {}'.format(mahasiswa[i][2], mahasiswa[i][0], mahasiswa[i][1], mahasiswa[i][3], mahasiswa[i][4]))
+                    break
+            else:
+                print('Data tidak ditemukan')
                 break
+        elif menu_pilih_show == '3':
+            return
         else:
-            print('Data tidak ditemukan')
-    else:
-        print('Pilihan tidak tersedia')
+            print('Pilihan tidak tersedia')
 
-
-
+        
 
 def inputMahasiswa():
     nama = input('Masukkan Nama[4-30]: ')
-    while not (nama.isalpha() and 4 <= len(nama) <= 30):
+    while not (nama.replace(" ", "").isalpha() and 4 <= len(nama) <= 30):
         print('Nama harus terdiri dari 4-30 karakter dan hanya berisi huruf')
         nama = input('Masukkan Nama[4-30]: ')
 
     jurusan = input('Masukkan Jurusan: ')
+    while not (jurusan.replace(" ", "").isalpha() and 4 <= len(jurusan) <= 40):
+        print('Jurusan harus terdiri dari 4-40 karakter dan hanya berisi huruf')
+        jurusan = input('Masukkan Jurusan: ')
+
     nim = input('Masukkan NIM: ')
-    while not (nim.startswith('B0') and len(nim) != 8):
-        print('NIM harus diawali dengan "B0" dan terdiri dari 8 karakter')
+    while len(nim) != 8 or not nim.startswith('20') or not nim[2:].isdigit():
+        print('NIM harus terdiri dari 8 karakter dan diawali dengan "20"')
         nim = input('Masukkan NIM: ')
+        nim = str(nim)
 
     nama_orang_tua = input('Masukkan Nama Orang Tua[4-30]: ')
-    while not (nama_orang_tua.isalpha() and 4 <= len(nama_orang_tua) <= 30):
+    while not (nama_orang_tua.replace(" ", "").isalpha() and 4 <= len(nama_orang_tua) <= 30):
         print('Nama Orang Tua harus terdiri dari 4-30 karakter dan hanya berisi huruf')
         nama_orang_tua = input('Masukkan Nama Orang Tua[4-30]: ')
 
@@ -66,44 +67,56 @@ def inputMahasiswa():
             break
         gpa = float(gpa)
 
-        mahasiswa.append([nama, jurusan, nim, nama_orang_tua, gpa])
-        showMahasiswa()
-
-   
-
-
-def updateDataMahasiswa():
-    print('''
-
-1.Update Jurusan
-2.Update GPA
-
-    ''')
-
-    pilih_update = int(input('Masukan Nomor: '))
-    if pilih_update == 1:
-        nim = input('Masukan NIM: ')
-        for i in range(len(mahasiswa)):
-            if nim in mahasiswa[i] :
-                jurusan_baru = input('Masukan Jurusan Baru: ')
-                mahasiswa[i][1] = jurusan_baru
-    elif pilih_update == 2:
-        nim = input('Masukan NIM: ')
-        for i in range(len(mahasiswa)):
-            if nim in mahasiswa[i]:
-                gpa_baru = input('Masukan GPA Baru: ')
-                mahasiswa[i][4] = gpa_baru
+    mahasiswa.append([nama, jurusan, nim, nama_orang_tua, gpa])
     showMahasiswa()
 
 
-def deleteDataMahasiswa():
-    nim = input('Masukan NIM: ')
+def updateMahasiswa():
+    nim = input("Masukkan NIM mahasiswa yang ingin diupdate: ")
     for i in range(len(mahasiswa)):
-        if nim in mahasiswa[i]:
-            del mahasiswa[i]
+        if nim == mahasiswa[i][2]:
+            while True:
+                print('''
+                1. Nama
+                2. Jurusan
+                3. Nama Wali
+                4. GPA
+                5. Kembali ke Menu Utama
+                ''')
+                menu_pilih_update = input('Pilih Menu (1/2/3/4/5): ')
+
+                if menu_pilih_update == '1':
+                    mahasiswa[i][0] = input("Masukkan nama baru: ")
+                    print("Data berhasil diupdate")
+                elif menu_pilih_update == '2':
+                    mahasiswa[i][1] = input("Masukkan jurusan baru: ")
+                    print("Data berhasil diupdate")
+                elif menu_pilih_update == '3':
+                    mahasiswa[i][3] = input("Masukkan nama wali baru: ")
+                    print("Data berhasil diupdate")
+                elif menu_pilih_update == '4':
+                    mahasiswa[i][4] = input("Masukkan GPA baru: ")
+                    print("Data berhasil diupdate")
+                elif menu_pilih_update == '5':
+                    return
+                else:
+                    print('Pilihan tidak tersedia')
             break
-    
-    showMahasiswa()
+    else:
+        print('Data tidak ditemukan')
+
+
+
+def deleteMahasiswa():
+    nim = input('Masukkan NIM Mahasiswa yang akan dihapus: ')
+    for i in range(len(mahasiswa)):
+        if nim == mahasiswa[i][2]:
+            del mahasiswa[i]
+            print('Data berhasil dihapus')
+            return
+    else:
+        print('Data tidak ditemukan')
+
 
 
     
@@ -111,7 +124,7 @@ def deleteDataMahasiswa():
 def menu():
     print(f'''
 
-"EMPLOYEES"
+"MAHASISWA"
 {'-' * 150}
 1.Show Mahasiswa Data
 2.Input Mahasiswa Data
@@ -127,11 +140,14 @@ while True:
     if menu_input == '1':
         showMahasiswa()
     elif menu_input == '2':
+        menu()
         inputMahasiswa()
     elif menu_input == '3':
-        updateDataMahasiswa()
+        menu()
+        updateMahasiswa()
     elif menu_input == '4':
-        deleteDataMahasiswa()
+        menu()
+        deleteMahasiswa()
     elif menu_input.lower() == 'quit':
         break
     else:
